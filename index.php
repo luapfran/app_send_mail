a736987e5317ad53c405b6819c0a09945e7148fc
Paulo José <32067190+luapfran@users.noreply.github.com>
2023-04-01 11:12:45 -0300

    atualizando o index com db

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
   <?php
+    #require('connect.php');
     require('connect.php');
   ?>
 
